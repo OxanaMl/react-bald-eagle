b1e8192730931820814e33c9082dba498dffbe73
Oksana M <kesusha.melnyk@gmail.com>
2023-02-19 04:01:58 -0800

    Improve project structure, add component PropTypes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AddTodoForm from "./AddTodoForm";
-import TodoList from "./TodoList";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 import style from "./App.module.css";
 
 const App = () => {
